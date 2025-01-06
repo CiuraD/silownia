d601c4e879a713574a060f745d0cb545073c4d8e
Kajetan Nowak <kajtek2n@gmail.com>
2025-01-06 17:09:03 +0100

    Correct bpmn diagrams

--- src/main/java/pl/gym/bpmn/demo/Controller.java
+++ src/main/java/pl/gym/bpmn/demo/Controller.java
@@ -1,14 +1,12 @@
 package pl.gym.bpmn.demo;
 
 import io.camunda.zeebe.client.ZeebeClient;
+import java.util.Map;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.Primary;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Map;
 
 @RestController
 @RequestMapping("/api/process")
@@ -25,12 +23,12 @@ public class Controller {
   public String startProcess(@RequestBody Map<String, Object> variables) {
     client
         .newCreateInstanceCommand()
-        .bpmnProcessId("order-process")
+        .bpmnProcessId("assign-training")
         .latestVersion()
         .variables(variables)
         .send()
         .join();
 
-    return "Process started successfully!";
+    return variables.toString();
   }
 }
